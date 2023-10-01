4c281315817fc0fd5d91d1a7b6b36d81be20166d
GiochiInVilla <146368201+GiochiInVilla@users.noreply.github.com>
2023-10-01 19:57:55 +0200

    Add files via upload

--- eventBox/eventBox.js
+++ eventBox/eventBox.js
@@ -2,7 +2,6 @@ function eventBox() {
 			var clsImage = document.getElementsByClassName("eventImage"); 
 			var clsName = document.getElementsByClassName("eventName"); 
 			var clsLink = document.getElementsByClassName("eventLink"); 
-			document.getElementById('testing').innerHTML = "[TEST2.2]";
 			for (n=0, length = clsImage.length; n < length; n++) {
 				clsImage[n].id= "eventImage_" + (n + 1); 
 				clsName[n].id= "eventLink_" + (n + 1); 
