f0d0947ab3fcbe3749878ab251024d398971e59c
GiochiInVilla <146368201+GiochiInVilla@users.noreply.github.com>
2023-10-02 14:37:48 +0200

    Add files via upload

--- eventBox/eventBox.js
+++ eventBox/eventBox.js
@@ -2,5 +2,7 @@ function eventBox() {
 	var iframe = document.getElementsByClassName("eventBoxFrame");
 	for (let i = 0; i < iframe.length; i++) {
 		iframe[i].contentWindow.document.getElementsByClassName('eventName')[0].setAttribute('id', "eventName_" + (i + 1));
+		iframe[i].contentWindow.document.getElementsByClassName('eventImage')[0].setAttribute('id', "eventImage_" + (i + 1));
+		iframe[i].contentWindow.document.getElementsByClassName('eventImage')[0].setAttribute('id', "eventLink_" + (i + 1));
 	}
 }
