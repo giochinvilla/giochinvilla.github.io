0bb23e97266a7b02c43ef993a20bc4cd2850cc9a
GiochiInVilla <146368201+GiochiInVilla@users.noreply.github.com>
2023-10-07 14:06:29 +0200

    Add files via upload

--- sideNav/sideNav.js
+++ sideNav/sideNav.js
@@ -1,14 +1,7 @@
 function openNav() {
   document.getElementById("sideNav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-  document.getElementById("header-logo").style.left = "300px";
-  document.getElementById("header").style.width = window.innerWidth-250 + "px";
 }
 
 function closeNav() {
   document.getElementById("sideNav").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
-  document.getElementById("header-logo").style.left = "50px";
-  document.getElementById("header").style.width = window.innerWidth + "px";
 }
