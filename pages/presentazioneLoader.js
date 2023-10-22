849120415cd3c3587ad4ab2cc00b4b3b1a593c61
GiochiInVilla <146368201+GiochiInVilla@users.noreply.github.com>
2023-10-22 19:24:10 +0200

    Add files via upload

---
+++ pages/presentazioneLoader.js
@@ -0,0 +1,16 @@
+function subloadEvents() {
+			document.getElementsByClassName("paragraphTitleTitle")[0].innerHTML = "Chi Siamo e cosa facciamo";
+			document.getElementsByClassName("paragraphSubtitleTitle")[0].innerHTML = "Dalle 20:30";
+			document.getElementsByClassName("paragraphSubtitleTitle")[1].innerHTML = "Dal 2018";
+			document.getElementsByClassName("paragraphSubtitleTitle")[2].innerHTML = "Ogni lunedi'";
+			document.getElementsByClassName("paragraphTitleTitle")[1].innerHTML = "Gioca con noi";
+			document.getElementsByClassName("paragraphTitleTitle")[2].innerHTML = "La presenza nel territorio";
+			document.getElementsByClassName("paragraphTitleTitle")[3].innerHTML = "Galleria";
+			document.getElementsByClassName("paragraphTitleTitle")[4].innerHTML = "Seguici e contattaci";
+			
+			showSlides(1);
+			
+			document.getElementsByClassName("eventImage")[0].src = "../images/rolegames_gallery/gallery1.jpg";
+			document.getElementsByClassName("eventImage")[1].src = "../images/rolegames_gallery/gallery2.jpg";
+			document.getElementsByClassName("eventImage")[2].src = "../images/rolegames_gallery/gallery3.jpg";
+		}
